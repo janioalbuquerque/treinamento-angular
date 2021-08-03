894c46e37b9c741db5e77ffa01a5a876d6f84fb4
janioalbuquerque <aurelio.janio@gmail.com>
2021-08-03 20:44:05 -0300

    Capturando eventos e o Output

--- src/app/barra-navegacao/barra-navegacao.component.ts
+++ src/app/barra-navegacao/barra-navegacao.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter  } from '@angular/core';
+
 
 @Component({
   selector: 'app-barra-navegacao',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BarraNavegacaoComponent implements OnInit {
 
+  @Output() onToggle = new EventEmitter();
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  disparaEvento(){
+    this.onToggle.emit();
   }
 
 }
